2877666dd5d7699a10f75eee0dda9ac73132e21e
Alex Behling <abehling@behling-it.de>
2019-04-07 20:32:08 +0200

    Add parameters to vga module for more flexibility

--- vga.v
+++ vga.v
@@ -1,28 +1,33 @@
 module vga (clk, reset, out, hsync, vsync);
 
+  parameter SCREEN_WIDTH = 640;
+  parameter SCREEN_HEIGHT = 480;
+  parameter V_POL = 1'b1;
+  parameter H_POL = 1'b1;
+  // Timing for horizontal front porch, pulse time and back porch
+  parameter H_FP = 16;
+  parameter H_SYNC = 96;
+  parameter H_BP = 48;
+  parameter H_MAX = H_FP + H_SYNC + H_BP + SCREEN_WIDTH;
+
+  // Timing for vertical front porch, pulse time and back porch
+  parameter V_FP = 10;
+  parameter V_SYNC = 2;
+  parameter V_BP = 33;
+  parameter V_MAX = V_FP + V_SYNC + V_BP + SCREEN_HEIGHT;
+
   input clk, reset;
   
   // The PMOD VGA connector has 4 Bit for each color
   output reg [11:0] out = 12'b000000000000;
 
   // for 640x480 both are active low
-  output reg hsync = 1'b1;
-  output reg vsync = 1'b1;
+  output reg hsync = H_POL;
+  output reg vsync = V_POL;
   
   reg [9:0] counterH = 0;
   reg [9:0] counterV = 0;
 
-  // Timing for horizontal front porch, pulse time and back porch
-  localparam H_FP = 16;
-  localparam H_SYNC = 96;
-  localparam H_BP = 48;
-  localparam H_MAX = H_FP + H_SYNC + H_BP + 640;
-
-  // Timing for vertical front porch, pulse time and back porch
-  localparam V_FP = 10;
-  localparam V_SYNC = 2;
-  localparam V_BP = 33;
-  localparam V_MAX = V_FP + V_SYNC + V_BP + 480;
 
   always @(posedge clk)
     begin
@@ -34,7 +39,7 @@ module vga (clk, reset, out, hsync, vsync);
         end
       else 
         begin
-          if (counterH > H_FP+H_SYNC+H_BP && counterH < H_FP+H_SYNC+H_BP+640)
+          if (counterH > H_FP+H_SYNC+H_BP && counterH < H_FP+H_SYNC+H_BP+SCREEN_WIDTH)
             begin
               // Now we are in the visible area
               // Pixel Format is RGB444
@@ -48,11 +53,11 @@ module vga (clk, reset, out, hsync, vsync);
             end
           else if (counterH == H_FP)
             // hsync pulse starts
-            hsync <= ~hsync;
+            hsync <= ~H_POL;
           else if (counterH == H_FP+H_SYNC)
             begin
-              // hsync pulse ends and line max is reached
-              hsync <= ~hsync;
+              // hsync pulse ends
+              hsync <= H_POL;
             end
           else if (counterH == H_MAX)
             begin
@@ -60,14 +65,12 @@ module vga (clk, reset, out, hsync, vsync);
               counterV <= counterV + 1;
             end
 
-          if (counterV == 480+V_FP)
+          if (counterV == SCREEN_HEIGHT+V_FP)
             // vsync pulse starts
-            vsync <= 1'b0;
-          if (counterV == 480+V_FP+V_SYNC)
-            begin
-              // vsync pulse ends and frame max is reached
-              vsync <= 1'b1;
-            end
+            vsync <= ~V_POL;
+          if (counterV == SCREEN_HEIGHT+V_FP+V_SYNC)
+            // vsync pulse ends
+            vsync <= V_POL;
           if (counterV == V_MAX)
             counterV <= 0;
           counterH <= counterH + 1;
