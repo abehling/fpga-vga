488370761e6b416513082f3503fd2375c5aa21cb
Alex Behling <abehling@behling-it.de>
2019-04-10 07:18:31 +0200

    Replace counter based clock with PLL driven clock

--- top_vga.v
+++ top_vga.v
@@ -7,28 +7,11 @@ module top_vga(clk, rgb_out, hsync, vsync);
   output wire hsync;
   output wire vsync;
 
-  reg [1:0] clkcounter = 2'b00;
   wire clk25;
-  assign clk25 = clkcounter[1];
-//  wire clk50;
-//  assign clk50 = clkcounter[0];
+  wire locked;
 
+  // This pll only generates 25 MHz clock, generated with icepll -i 100 -o 25 -m -f pll.v
+  pll pll25(clk, clk25, locked);
   vga display_controller(clk25, reset, rgb_out, hsync, vsync);
 
-//  vga #(.V_POL(1'b0),
-//        .H_POL(1'b0),
-//        .SCREEN_WIDTH(800),
-//        .SCREEN_HEIGHT(600),
-//        .H_FP(56),
-//        .H_SYNC(120),
-//        .H_BP(64),
-//        .V_FP(37),
-//        .V_SYNC(6),
-//        .V_BP(23)) display_controller(clk50, reset, rgb_out, hsync, vsync);
-
-  // counter, until I understand PLL
-  always @(posedge clk)
-  begin
-    clkcounter <= clkcounter + 1;
-  end
 endmodule
